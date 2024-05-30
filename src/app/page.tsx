20cecf6cabf3eb7c78114c0ce3302eb02b9298c3
engelbert teh <engelbertteh.dev@gmail.com>
2024-05-30 22:41:23 +0800

    completed ui for editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import CodeEditor from '@/components/component/CodeEditor';
 export default function Home() {
   return (
     <main>
-      <div className="h-screen w-screen bg-white flex justify-center items-center">
+      <div className="h-screen w-screen bg-white flex flex-col justify-center items-center">
+        <h1 className={'text-black mb-4 font-semibold'}>JAVA ASSIGNMENT 01:</h1>
         <CodeEditor />
       </div>
     </main>
--- src/components/component/CodeEditor.tsx
+++ src/components/component/CodeEditor.tsx
@@ -1,28 +1,44 @@
 'use client';
 function CodeEditor() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center">
       <form
+        id="myForm"
         onSubmit={(event) => {
           // handle form submit
           event.preventDefault();
-          console.log('submitted');
+          console.log('pressed');
+          // call hackerearth api
         }}
-        className="bg-slate-200 h-[50dvh] p-2 mx-4 w-full lg:w-[1000px] rounded-md"
+        className="bg-slate-200 h-[60dvh] p-2 mx-4 w-full lg:w-[1000px] rounded-md relative overflow-hidden"
       >
-        <textarea
-          style={{ resize: 'none', outline: 'none' }}
-          className="w-full h-full bg-gray-200 p-2 text-black"
-          placeholder="Write your code here..."
-        ></textarea>
+        <div className="h-[95%] py-2">
+          <p className="text-slate-600">class Program &#123;</p>
+          <p className="text-slate-600 px-[2rem]">
+            public static void main &#40; String&#91; &#93; args &#41; &#123;
+          </p>
+
+          <textarea
+            style={{
+              resize: 'none',
+              outline: 'none',
+              height: 'calc(100% - 4rem)',
+            }}
+            className="w-full h-full bg-gray-200 my-2 py-[1rem] px-[4rem]  text-slate-600"
+            placeholder="Write your code here..."
+          ></textarea>
+
+          <p className="text-slate-600"> &#125;</p>
+        </div>
       </form>
       <button
         type={'submit'}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        form={'myForm'}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded   mt-[2rem] w-[200px] lg:w-[500px]  "
       >
         Submit
       </button>
-    </>
+    </div>
   );
 }
 
