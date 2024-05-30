bf6789d15b268b4086cdc056dfd13aefe1c819a2
engelbert teh <engelbertteh.dev@gmail.com>
2024-05-31 04:12:32 +0800

    sysout able

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import CodeEditor from '@/components/component/CodeEditor';
-
 export default function Home() {
   return (
     <main>
-      <div className="h-screen w-screen bg-white flex flex-col justify-center items-center">
-        <h1 className={'text-black mb-4 font-semibold'}>JAVA ASSIGNMENT 01:</h1>
+      <div className="bg-white flex justify-center items-center mt-[2rem]">
+        <h1 className={'text-black  font-semibold'}>JAVA ASSIGNMENT 01:</h1>
+      </div>
+      <div className=" mx-[1rem] h-screen w-screen bg-white flex flex-col justify-center items-center ">
         <CodeEditor />
       </div>
     </main>
--- src/components/component/CodeEditor.tsx
+++ src/components/component/CodeEditor.tsx
@@ -1,16 +1,97 @@
 'use client';
+import { FormEvent, useRef, useState } from 'react';
+import { useSWRConfig } from 'swr';
+interface SubmissionBody {
+  lang: string;
+  source: string;
+  input: string;
+  memory_limit: number;
+  time_limit: number;
+  context?: string; // Optional
+  callback: string;
+}
 function CodeEditor() {
+  const { mutate } = useSWRConfig();
+  const [results, setResults] = useState<{ output: string } | undefined>(
+    undefined
+  );
+  const [awaitedResults, setAwaitedResults] = useState<JSON | undefined>(
+    undefined
+  );
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const submissionBody: SubmissionBody = {
+    lang: 'JAVA14',
+    source: '',
+    input: '', // Adjust if input is required
+    memory_limit: 243232,
+    time_limit: 5,
+    context: '', // Optional, leave empty if not needed
+    callback: 'https://client.com/callback/',
+  };
+  const handleForm = async (e: FormEvent) => {
+    e.preventDefault();
+    const data = textAreaRef.current?.value;
+
+    const JSONdata = `public class Program { public static void main ( String[] args ) { ${data} } }`;
+
+    submissionBody.source = JSONdata;
+
+    const endpoint =
+      'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Secret': '31eb3af969a685ff525f14ccabe8862c7279bab1',
+      },
+      body: JSON.stringify(submissionBody),
+    };
+    try {
+      const postResponse = await fetch(endpoint, options);
+
+      if (!postResponse.ok) {
+        const errorData = await postResponse.json();
+        console.error('Error submitting code:', errorData);
+        return;
+      }
+      const result: { status_update_url: string } = await postResponse.json();
+
+      console.log(result.status_update_url);
+      let getResponseData = null;
+      do {
+        const getResponse = await fetch(result.status_update_url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Client-Secret': '31eb3af969a685ff525f14ccabe8862c7279bab1',
+          },
+        });
+
+        getResponseData = await getResponse.json();
+      } while (getResponseData.request_status.code !== 'REQUEST_COMPLETED');
+      let resp = null;
+      do {
+        resp = await fetch(getResponseData.result.run_status.output);
+      } while (resp.status !== 200);
+
+      console.log(resp.status);
+      const blob = await resp.blob();
+      const text = await blob.text();
+      console.log(text.replaceAll('\n', ''));
+    } catch (error) {
+      console.log(error);
+    }
+
+    // mutate(
+    //   'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/'
+    // );
+  };
   return (
     <div className="flex flex-col items-center justify-center">
       <form
         id="myForm"
-        onSubmit={(event) => {
-          // handle form submit
-          event.preventDefault();
-          console.log('pressed');
-          // call hackerearth api
-        }}
-        className="bg-slate-200 h-[60dvh] p-2 mx-4 w-full lg:w-[1000px] rounded-md relative overflow-hidden"
+        onSubmit={handleForm}
+        className="bg-slate-200 h-[60dvh] p-2 w-[350px] sm:w-[600px] lg:w-[1000px] rounded-md relative overflow-hidden"
       >
         <div className="h-[95%] py-2">
           <p className="text-slate-600">class Program &#123;</p>
@@ -19,11 +100,13 @@ function CodeEditor() {
           </p>
 
           <textarea
+            ref={textAreaRef}
             style={{
               resize: 'none',
               outline: 'none',
               height: 'calc(100% - 4rem)',
             }}
+            spellCheck={false}
             className="w-full h-full bg-gray-200 my-2 py-[1rem] px-[4rem]  text-slate-600"
             placeholder="Write your code here..."
           ></textarea>
@@ -38,6 +121,16 @@ function CodeEditor() {
       >
         Submit
       </button>
+
+      <textarea
+        className={
+          'bg-slate-200 pt-4  m-[2rem] rounded-md w-full h-[300px] px-[2rem]'
+        }
+        hidden={(results && JSON.stringify(results)) == null}
+        disabled={true}
+        defaultValue={(results && JSON.stringify(results)) ?? ''}
+        placeholder="Output"
+      ></textarea>
     </div>
   );
 }
