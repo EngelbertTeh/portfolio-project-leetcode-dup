0365082597fc44d827f603b11a54135ef12c2012
engelbert teh <engelbertteh.dev@gmail.com>
2024-06-10 22:38:39 +0800

    added pattern for for email input

---
+++ src/app/(IAM)/register/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { useRef } from 'react';
+
+function Register() {
+  const formRef = useRef<HTMLFormElement>(null);
+  return (
+    <div className=" bg-[#000c19] h-[100dvh] w-[100dvwq] flex justify-center items-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Register with your gmail</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form ref={formRef} action="api/users/">
+            <input
+              type="text"
+              placeholder="Email"
+              pattern="^[a-zA-Z0-9]+(.[a-zA-Z0-9]+){0,1}@[gG][mM][aA][iI][lL].[cC][oO][mM]$"
+              required
+              title="Enter valid gmail address"
+            />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Register</button>
+          </form>
+        </CardContent>
+        <CardFooter className="p-4 pr-2 gap-2  w-full flex justify-end">
+          <Button
+            onClick={() => {
+              formRef.current?.submit();
+            }}
+          >
+            Register
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default Register;
