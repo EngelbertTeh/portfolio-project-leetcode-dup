b4ce40f03de02bc744bc94a79ab482e5110b3378
engelbert teh <engelbertteh.dev@gmail.com>
2024-06-12 21:42:49 +0800

    fixed unable to deploy issue

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -39,7 +39,7 @@ export async function GET(req: NextRequest) {
 }
 
 // UPDATE
-export async function updateUser(email: string, password: string) {}
+// export async function updateUser(email: string, password: string) {}
 
 // DELETE
 
